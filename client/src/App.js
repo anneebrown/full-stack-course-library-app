cb7d8afce395e3c7bd3996b613a4dc8d9b7b317c
anneebrown <61443022+anneebrown@users.noreply.github.com>
2020-08-19 23:08:21 -0700

    course and coursedetail are functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import Courses from './Courses'
+import Header from './Header'
+import CourseDetail from './CourseDetail'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
+      <Header />
       </header>
-      <Courses />
+      <Switch>
+        <Route exact path="/" component={Courses} />
+        <Route exact path="/courses/:id" component={CourseDetail} />
+      </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/CourseDetail.js
+++ client/src/CourseDetail.js
@@ -8,56 +8,69 @@ export default class CourseDetail extends Component {
   constructor() {
     super();
     this.state = {
-      courses: []
+      course: [],
+      user: []
     };
   } 
+
+  consoleLog(){
+  console.log(this.props.match.params)
+  }
   
   //gets the course from the api
   retrieveCourses() {
-    axios.get(`http://localhost:5000/api/courses/${id}`)
-    .then(response => this.setState({courses: response.data.courses}))
+    axios.get(`http://localhost:5000/api/courses/${this.props.match.params.id}`)
+    .then(response => this.setState({course: response.data.course, user: response.data.user}))
     .catch(error => {
         console.log('Error fetching and parsing data', error);
     });    
   } 
 
-  //loops over the courses, creates html elements for each course and stores the elements in an array
-  pushCourses() {
-    let htmlCourses = [];
-    for (let i = 0; i < this.state.courses.length; i++) {
-     htmlCourses.push(<div className="grid-33" key={this.state.courses[i].id}><a className="course--module course--link" href={'/courses/' + this.state.courses[i].id}>
-      <h4 className="course--label">Course</h4>
-      <h3 className="course--title">{this.state.courses[i].title}</h3>
-      </a></div>
-     )
-    }
-    return htmlCourses;
-  }
-
   componentDidMount() {
      this.retrieveCourses();
-     this.pushCourses();
+     //this.consoleLog();
   }
 
   render() {
-    console.log(this.state.courses)
+    console.log(this.state.course)
     return (
       <div className="bounds">
-      {this.pushCourses()}
-        {/* {
-            this.state.courses.map(course => (
-              <div className="grid-33" key={course.id}><a className="course--module course--link" href="/courses/"+{course.id}>
+              <div>
+        <div className="actions--bar">
+          <div className="bounds">
+            <div className="grid-100"><span><a className="button" href={'/courses/' + this.props.match.params.id + '/update'}>Update Course</a><a className="button" href="/">Delete Course</a></span><a
+                className="button button-secondary" href="/">Return to List</a></div>
+          </div>
+        </div>
+        <div className="bounds course--detail">
+          <div className="grid-66">
+            <div className="course--header">
               <h4 className="course--label">Course</h4>
-              <h3 className="course--title">{course.title}</h3>
-              </a></div>
-          ))
-        } */}
-        <div className="grid-33"><a className="course--module course--add--module" href="/courses/create">
-            <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                viewBox="0 0 13 13" className="add">
-                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
-              </svg>New Course</h3>
-          </a></div>
+              <h3 className="course--title">{this.state.course.title}</h3>
+              <p>By {this.state.user.firstName} {this.state.user.lastName}</p>
+            </div>
+            <div className="course--description">
+              <p>{this.state.course.description}</p>
+            </div>
+          </div>
+          <div className="grid-25 grid-right">
+            <div className="course--stats">
+              <ul className="course--stats--list">
+                <li className="course--stats--list--item">
+                  <h4>Estimated Time</h4>
+                  <h3>{this.state.course.estimatedTime}</h3>
+                </li>
+                <li className="course--stats--list--item">
+                  <h4>Materials Needed</h4>
+                  <ul>
+                    <li>{this.state.course.materialsNeeded}</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     )
   };
